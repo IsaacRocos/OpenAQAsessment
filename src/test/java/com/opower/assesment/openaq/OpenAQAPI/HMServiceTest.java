f766d35603ce645a08d59a297c8b952bbb5cbdc1
Ángel Isaac Rodríguez Cosme <angel.rodriguez@mitec.com.mx>
2022-05-11 03:45:42 -0500

    Added some unit tests

--- src/test/java/com/opower/assesment/openaq/OpenAQAPI/HMServiceTest.java
+++ src/test/java/com/opower/assesment/openaq/OpenAQAPI/HMServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.opower.assesment.openaq.OpenAQAPI.constants.AirQualityParameter;
 import com.opower.assesment.openaq.OpenAQAPI.schemas.LocationsHeatMap;
+import com.opower.assesment.openaq.OpenAQAPI.schemas.MeasurementsHeatmap;
 import com.opower.assesment.openaq.OpenAQAPI.services.HMService;
 
 @SpringBootTest
@@ -30,6 +31,16 @@ public class HMServiceTest {
 		assertEquals(0, lhm.getMeta().getFound());
 	}
 	
+	@Test
+	void testGetHMMeasurements() throws MalformedURLException, URISyntaxException {
+		
+		//Correct call
+		MeasurementsHeatmap mhm = hmService.getHMMeasurements("MX", AirQualityParameter.PM25.getName(), "19.4858249,-99.1995944", 10000 );
+		assertNotEquals(0, mhm.getMeta().getFound());
+		
+		// Incorrect call
+		mhm = hmService.getHMMeasurements("WW", AirQualityParameter.PM25.getName(), "19.4858249,-99.1995944", 1000 );
+		assertEquals(0, mhm.getMeta().getFound());
+	}
 	
-
 }
--- src/test/java/com/opower/assesment/openaq/OpenAQAPI/OpenAqapiApplicationTests.java
+++ src/test/java/com/opower/assesment/openaq/OpenAQAPI/OpenAqapiApplicationTests.java
@@ -1,13 +1,25 @@
 package com.opower.assesment.openaq.OpenAQAPI;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OpenAqapiApplicationTests {
 
+
+	@Value("${url.base.locations}")
+	private String locationsBaseUrl;
+
+	@Value("${url.base.measurements}")
+	private String measurementsBaseUrl;
+	
 	@Test
 	void contextLoads() {
+		assertNotNull(locationsBaseUrl);
+		assertNotNull(measurementsBaseUrl);
 	}
 
 }
